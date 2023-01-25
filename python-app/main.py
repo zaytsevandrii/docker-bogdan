68bf152548edd3a304f4a7cbeacb0057789ad02e
Andrii Zaitsev <105815678+zaytsevandrii@users.noreply.github.com>
2023-01-25 17:10:38 +0000

    add 2app python and node

---
+++ python-app/main.py
@@ -0,0 +1,10 @@
+import calendar
+
+print('Wlcome to my SUPER calendar\n')
+
+year = int(input('Please enter your city:'))
+month = int(input('Please enter your month:'))
+
+print(calendar.month(year,month))
+
+print('All the best!')
